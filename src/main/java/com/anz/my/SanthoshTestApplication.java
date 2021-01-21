c7457ca9200388abcf25fd107d5f18a4265b9898
Hirekerur, Santhosh <Santhosh.Hirekerur@CookMedical.com>
2021-01-21 19:01:05 +1000

    second commit

--- src/main/java/com/anz/my/SanthoshTestApplication.java
+++ src/main/java/com/anz/my/SanthoshTestApplication.java
@@ -2,12 +2,24 @@ package com.anz.my;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class SanthoshTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SanthoshTestApplication.class, args);
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer propsConfig = new PropertySourcesPlaceholderConfigurer();
+		propsConfig.setLocation(new ClassPathResource("git.properties"));
+		propsConfig.setIgnoreResourceNotFound(true);
+		propsConfig.setIgnoreUnresolvablePlaceholders(true);
+		return propsConfig;
 	}
 
 }
--- src/main/java/com/anz/my/controller/GitInfoController.java
+++ src/main/java/com/anz/my/controller/GitInfoController.java
@@ -1,13 +1,7 @@
 package com.anz.my.controller;
 
-import java.io.File;
-
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.InvalidRemoteException;
-import org.eclipse.jgit.api.errors.TransportException;
-import org.eclipse.jgit.revwalk.RevCommit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,26 +13,16 @@ public class GitInfoController {
 
 	@Autowired
 	BuildProperties buildProperties;
+
+	@Value("${git.commit.id}")
+	private String commitId;
 	
-	//@Value("${GIT_URL}")
-	String GIT_URL;
-	
-	//@Value("${GIT_DIR}")
-	String GIT_DIR;
-	
+	@Value("${git.commit.message.short}")
+	private String message;
 	
 	@GetMapping("/version")
-	public GitInfo getVersionInfo() throws InvalidRemoteException, TransportException, GitAPIException {
-
-		/*
-		 * File directory = new File(GIT_DIR); Git repository =
-		 * Git.cloneRepository().setURI(GIT_URL).setDirectory(directory).call();
-		 * RevCommit latestCommit =
-		 * repository.log().setMaxCount(1).call().iterator().next();
-		 */
-		
-		
-		return new GitInfo(buildProperties.getVersion(), "test", "test");
+	public GitInfo getVersionInfo() {
+		return new GitInfo(buildProperties.getVersion(), commitId, message);
 	}
 
 }
